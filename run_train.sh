ddeb678ec495cd206110c11a1564ab50541f3955
Yuxuan Zhang <39586897+yxzwayne@users.noreply.github.com>
2024-01-06 21:39:10 -0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -5,35 +5,34 @@
 #conda activate tf-gpu
 #export PYTHONUNBUFFERED=1
 
-latent8=8
-latent30=30
+seq_file=seq/6M.exper.seqs
 gen_size=1048576
 
 # ====== simple VAE from GPSA ======
 
-size=10K
+model_name=simpleVAE
 
-echo -e "\n\nTrain"
-python vaes.py simpleVAE_10k train sVAE 6M.exper_10k.seqs 7 250
-echo -e "\n\nPlot latent"
-python vaes.py simpleVAE_10k plot_latent 6M.exper_10k.seqs
-echo -e "\n\nGen 6M"
-python vaes.py simpleVAE_10k gen 10000 -o gen_simpleVAE_10k
+echo -e "\n\n===== Train"
+python vae_keras.py simpleVAE train sVAE $seq_file 7 250
+echo -e "\n\n===== Plot latent"
+python vae_keras.py simpleVAE plot_latent $seq_file
+echo -e "\n\n===== Generating sequences"
+python vae_keras.py simpleVAE gen $gen_size -o gen_simpleVAE_{$gen_size}
 
 
 # ====== DeepSequence ======
 
-echo -e "\n\nTrain"
-python vaes.py deepsequence_latent8 train DeepSequence 6M.exper_10k.seqs 8 --epoch 12
-echo -e "\n\nPlot latent"
-python vaes.py deepsequence_latent8 plot_latent 6M.exper_10k.seqs
-echo -e "\n\nGen 6M"
-python vaes.py deepsequence_latent8 gen 10000 -o gen_deepsequence_latent8
+# echo -e "\n\nTrain"
+# python vae_keras.py deepsequence_latent8 train DeepSequence $seq_file 8 --epoch 12
+# echo -e "\n\nPlot latent"
+# python vae_keras.py deepsequence_latent8 plot_latent $seq_file
+# echo -e "\n\nGen 6M"
+# python vae_keras.py deepsequence_latent8 gen $gen_size -o gen_deepsequence_latent8
 
 
-echo -e "\n\nTrain"
-python vaes.py deepsequence_latent30 train DeepSequence 6M.exper_10k.seqs 30 --epoch 32
-echo -e "\n\nPlot latent"
-python vaes.py deepsequence_latent30 plot_latent 6M.exper_10k.seqs
-echo -e "\n\nGen 6M"
-python vaes.py deepsequence_latent30 gen 10000 -o gen_deepsequence_latent30
+# echo -e "\n\nTrain"
+# python vae_keras.py deepsequence_latent30 train DeepSequence $seq_file 30 --epoch 32
+# echo -e "\n\nPlot latent"
+# python vae_keras.py deepsequence_latent30 plot_latent $seq_file
+# echo -e "\n\nGen 6M"
+# python vae_keras.py deepsequence_latent30 gen $gen_size -o gen_deepsequence_latent30
